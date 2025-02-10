88ce1fe0319c30ce59e4cbaac333bc81a38f561a
Daniel Durnea <21230183+danieldurnea@users.noreply.github.com>
2025-02-11 00:30:18 +0200

    Update docker-to-bash.sh

--- docker-to-bash.sh
+++ docker-to-bash.sh
@@ -9,25 +9,38 @@ sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo hostname $LINUX_MACHINE_NAME
 
 if [[ -z "$NGROK_AUTH_TOKEN" ]]; then
-  echo "Please set 'NGROK_AUTH_TOKEN'"
+  echo "No se ha detectado ningún 'NGROK_AUTH_TOKEN' por favor colocalo para poder seguir."
   exit 2
 fi
 
 if [[ -z "$LINUX_USER_PASSWORD" ]]; then
-  echo "Please set 'LINUX_USER_PASSWORD' for user: $USER"
+  echo "No se ha detectado ningun 'LINUX_USER_PASSWORD' por favor colocalo para poder seguir: $USER"
   exit 3
 fi
 
-echo "### Install ngrok ###"
+if [[ -z "$GIT_EMAIL" ]]; then
+echo "El correo de github es obligatorio, y no se ha encontrado por ninguna parte."
+exit 4
+fi
 
-wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux.zip
-unzip ngrok-stable-linux.zip
+if [[ -z "$GIT_NAME" ]]; then
+echo "El nombre es obligatorio, y no se ha encotrado por ninguna parte"
+exit 5
+fi
+
+bash linux-ssh.sh 
+
+echo "### Instalando ngrok para el tunel ###"
+
+wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
+unzip ngrok-stable-linux-386.zip
 chmod +x ./ngrok
 
-echo "### Update user: $USER password ###"
+echo "### Actualizando contraseña de: $USER ###"
 echo -e "$LINUX_USER_PASSWORD\n$LINUX_USER_PASSWORD" | sudo passwd "$USER"
+echo sshpass -p $LINUX_USER_PASSWORD ssh $LINUX_USERNAME@$LINUX_MACHINE_NAME
 
-echo "### Start ngrok proxy for 22 port ###"
+echo "### Iniciando el servidor vps. ###"
 
 
 rm -f .ngrok.log
@@ -35,14 +48,16 @@ rm -f .ngrok.log
 ./ngrok tcp 22 --log ".ngrok.log" &
 
 sleep 10
-HAS_ERRORS=$(grep "command failed" < .ngrok.log)
+HAS_ERRORS=$(grep "Lo sentimos, pero ha ocurrido un error insperado." < .ngrok.log)
 
 if [[ -z "$HAS_ERRORS" ]]; then
   echo ""
   echo "=========================================="
-  echo "To connect: $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh $USER@/" | sed "s/:/ -p /")"
-  echo "or conenct with $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh (Your Linux Username)@/" | sed "s/:/ -p /")"
+  echo "Para conectarte utiliza: $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh $USER@/" | sed "s/:/ -p /")"
+  echo "o para conectar con $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh $USER@/" | sed "s/:/ -p /")"
   echo "=========================================="
+  
+  
 else
   echo "$HAS_ERRORS"
   exit 4
